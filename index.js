de2e2dface5fc9876dc54221e54daf9d8927c94c
MariiaC <mariiachvorun@gmail.com>
2023-03-21 19:26:36 +0200

    project-init

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+
+// создаем екземпляр приложения
+const app = express();
+
+const PORT = process.env.PORT;
+
+const startApp = async () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server runs on PORT ${PORT}`)
+        })
+    } catch (e) {
+        console.log(e)
+        
+    }
+};
